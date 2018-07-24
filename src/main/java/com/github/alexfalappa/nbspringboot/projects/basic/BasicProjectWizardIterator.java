9578ffd4d02dab2b56c8db9b068103c7f362491a
Alessandro Falappa <alex.falappa@gmail.com>
2018-07-24 20:07:16 +0200

    Upgrade Spring Boot version to 2.0.3 in basic project wizard.

--- src/main/java/com/github/alexfalappa/nbspringboot/projects/basic/BasicProjectWizardIterator.java
+++ src/main/java/com/github/alexfalappa/nbspringboot/projects/basic/BasicProjectWizardIterator.java
@@ -69,7 +69,7 @@ import static com.github.alexfalappa.nbspringboot.PrefConstants.PREF_MANUAL_REST
 @Messages("BasicSpringbootProject_displayName=Spring Boot basic project")
 public class BasicProjectWizardIterator implements WizardDescriptor.ProgressInstantiatingIterator {
 
-    private static final String BOOTVERSION = "2.0.0.RELEASE";
+    private static final String BOOTVERSION = "2.0.3.RELEASE";
 
     private int index;
     private WizardDescriptor.Panel[] panels;
